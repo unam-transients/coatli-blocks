670d32b0abaf6764c353afe152e90f458d540b0b
Alan Watson <128939772+alanwatsonatwork@users.noreply.github.com>
2022-02-15 17:55:03 -0600

    Handle directories when fetching.

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-(
-  cd $(dirname "$0")/..
-  git clone https://github.com/alanwatsonforster/coatlioan-blocks.git
-)
+cd $(dirname "$0")/..
+
+rm -rf blocks.new
+mkdir blocks.new
+git clone https://github.com/alanwatsonforster/coatlioan-blocks.git blocks.new/
+
+rm -rf blocks.old
+mv blocks blocks.old
+mv blocks.new blocks
