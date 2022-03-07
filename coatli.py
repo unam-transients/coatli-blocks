d529a01f031ed2895e52d14e4f1ab507057b97b8
Diego Gonzalez <dihegobu@gmail.com>
2022-03-07 11:52:42 -0800

    Update coatli.py

--- coatli.py
+++ coatli.py
@@ -1,120 +1,69 @@
 import json
-
-inden = input('Enter a identifier (2002):')
+  
+inden = input('Enter a identifier:')
 NameA = input('Enter research name: ')
-NameB = input('Enter object name: ')
+NameB = input('Enter Name object: ')
 RA = input('Enter RA (00:00:00.00): ')
 Dec = input('Enter Dec (+00:00:00.00): ')
 comm = input('Enter command (gridvisit 1 5 1 60 {g r i z} false): ')
-c = int(input('Enter count number (1 or 2): '))
 
-count = c
-
-if count < 2:
-    dictionary ={
-        "project": {
-          "identifier": inden,
-          "name": NameA
-        },
-        "identifier": "0",
-        "name": NameB,
-        "visits": [
-          {
-            "identifier": "1000",
-            "name": "pointing correction",
-            "targetcoordinates": {
-              "type"   : "equatorial",
-              "alpha"  : RA,
-              "delta"  : Dec,
-              "equinox": "2000"
-            },
-            "command": "pointingcorrectionvisit",
-            "estimatedduration": "1m"
-          },
-          {
-            "identifier": "0",
-            "name": "science",
-            "targetcoordinates": {
-              "type"   : "equatorial",
-              "alpha"  : RA,
-              "delta"  : Dec,
-              "equinox": "2000"
-            },
-            "command": comm,
-            "estimatedduration": "20m"
-          }
-        ],
-        "constraints": {
-          "maxskybrightness": "bright",
-          "maxairmass": "1.5",
-          "minmoondistance": "15d"
-        },
-        "persistent": "false"
-    }
-elif count == 2:
-    dictionary ={
-        "project": {
-          "identifier": inden,
-          "name": NameA
-        },
-        "identifier": "0",
-        "name": NameB,
-        "visits": [
-          {
-            "identifier": "1000",
-            "name": "pointing correction",
-            "targetcoordinates": {
-              "type"   : "equatorial",
-              "alpha"  : RA,
-              "delta"  : Dec,
-              "equinox": "2000"
-            },
-            "command": "pointingcorrectionvisit",
-            "estimatedduration": "1m"
-          },
-          {
-            "identifier": "1001",
-            "name": "focus",
-            "targetcoordinates": {
-              "type"   : "equatorial",
-              "alpha"  : RA,
-              "delta"  : Dec,
-              "equinox": "2000"
-            },
-            "command": "focusvisit",
-            "estimatedduration": "2m"
-          },
-          {
-            "identifier": "0",
-            "name": "science",
-            "targetcoordinates": {
-              "type"   : "equatorial",
-              "alpha"  : RA,
-              "delta"  : Dec,
-              "equinox": "2000"
-            },
-            "command": comm,
-            "estimatedduration": "20m"
-          }
-        ],
-        "constraints": {
-          "maxskybrightness": "bright",
-          "maxairmass": "1.5",
-          "minmoondistance": "15d"
-        },
-        "persistent": "false"
+# Data to be written
+dictionary ={
+    "project": {
+      "identifier": inden,
+      "name": NameA
     },
-#else:
-#    print("Your number is very big")
-#    with open('data.json', 'w') as outfile:
-#        json.dump(dictionary, outfile)
-#    count = count -1;
-
+    "identifier": "0",
+    "name": NameB,
+    "visits": [
+      {
+        "identifier": "1000",
+        "name": "pointing correction",
+        "targetcoordinates": {
+          "type"   : "equatorial",
+          "alpha"  : RA,
+          "delta"  : Dec,
+          "equinox": "2000"
+        },
+        "command": "pointingcorrectionvisit",
+        "estimatedduration": "1m"
+      },
+      {
+        "identifier": "1001",
+        "name": "focus",
+        "targetcoordinates": {
+          "type"   : "equatorial",
+          "alpha"  : RA,
+          "delta"  : Dec,
+          "equinox": "2000"
+        },
+        "command": "focusvisit",
+        "estimatedduration": "2m"
+      },
+      {
+        "identifier": "0",
+        "name": "science",
+        "targetcoordinates": {
+          "type"   : "equatorial",
+          "alpha"  : RA,
+          "delta"  : Dec,
+          "equinox": "2000"
+        },
+        "command": comm,
+        "estimatedduration": "10m"
+      }
+    ],
+    "constraints": {
+      "maxskybrightness": "bright",
+      "maxairmass": "1.5",
+      "minmoondistance": "15d"
+    },
+    "persistent": "false"
+}
+  
 # Serializing json
-    json_object = json.dumps(dictionary, indent = 1)
+json_object = json.dumps(dictionary, indent = 4)
   
 # Writing to sample.json
-    with open("sample.json", "w") as outfile:
-        outfile.write(json_object)
-        count = count -1;
-
+with open("sample.json", "w") as outfile:
+    outfile.write(json_object)
